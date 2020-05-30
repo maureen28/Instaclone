13b5bbf1a1fdbbebe93ac9c100c9508183a043b9
Maureen <murimimaureen69@gmail.com>
2020-05-30 13:43:36 +0300

    Create get comments function

--- gram/urls.py
+++ gram/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views
-from ig.views import welcome, search_results, get_comments, messages
+from django.conf import settings
+from django.conf.urls.static import static
+
+from insta.views import welcome, search_results, get_comments, messages
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,4 +30,5 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$',views.logout, {"next_page": '/'}),
 ]
-
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

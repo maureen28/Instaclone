b9ae7dfb847e4ffd84673301a532b7f97ff62420
Maureen <murimimaureen69@gmail.com>
2020-05-30 13:26:47 +0300

    Create profile class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    

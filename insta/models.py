32f70b0f099cb3cfb80631fc2a4f2fbc456a68db
Maureen <murimimaureen69@gmail.com>
2020-05-30 13:27:34 +0300

    Add reciever

--- insta/models.py
+++ insta/models.py
@@ -5,8 +5,23 @@ class Profile(models.Model):
     profilePic = models.ImageField(upload_to='profile/',null=True)
     bio = models.CharField(max_length=60,blank=True)
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
